ad6fac3e32137d6f1cb5c71e1933732ebf2686b5
lydiaralexander <89818941+lydiaralexander@users.noreply.github.com>
2021-10-08 17:01:50 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -7,23 +7,74 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#1. The total number of votes cast
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
     # To do: read and analyze the date here.
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    #print each row in the CSV file.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    #print each row in the CSV file.
+    for row in file_reader:
+        #2. Add to the total vote count.
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+            
+        candidate_votes[candidate_name] += 1
+    
+    print(candidate_votes)
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})/n")
+
+        if(votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
 
 
 
 
 
 
-#1. The total number of votes cast
+
+
+
+
+
+
+
+
+
+
+
+
+
 #2. A complete list of candidates who received votes
 #3. The percentage of voties each candidate won
 #4. The total number of votes each candidate won
